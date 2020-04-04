5a7c6aadeb7ba28e030d354c760e3d5294c3cbad
join_li <shang_guan_feng@126.com>
2020-04-04 17:00:52 +0800

    login is ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from "@/components/login"
+import home from "@/components/home"
 Vue.use(Router)
 
 export default new Router({
@@ -13,6 +14,11 @@ export default new Router({
       path:"/login",
       name:"login",
       component:login
+    },
+    {
+      path:"/home",
+      name:"home",
+      component:home
     }
   ]
 })
